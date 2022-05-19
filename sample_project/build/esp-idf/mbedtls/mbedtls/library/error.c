1ffbc0580c9bff87361113bbf69b43e3afc4d00f
Helpsen <xiaosheng.chen@gl-inet.com>
2022-05-19 10:06:26 +0800

    delete sample file.

--- sample_project/build/esp-idf/mbedtls/mbedtls/library/error.c
+++
@@ -1,1 +0,0 @@
-/home/helpsen/esp/esp-idf/components/mbedtls/mbedtls/library/error.c
--- sample_project/main/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <time.h>
-void app_main(void)
-{
-	printf("test\n");
-	sleep(1);
-	printf("hello\n");
-	sleep(1);
-}
